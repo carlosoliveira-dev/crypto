a5ee30a8cb451deae359dee3072ebef8f63f11d8
code <carlos.oliveira.dev.junior@gmail.com>
2025-05-20 19:18:10 -0300

    rota post criada

--- src/server/main.js
+++ src/server/main.js
@@ -3,9 +3,15 @@ import ViteExpress from "vite-express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/hello", (req, res) => {
   res.send("Hello Vite!");
 });
+
+app.post('/cadastrar', (req, res) => {
+  res.send("request recebido!");
+})
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
