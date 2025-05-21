6d10c0e23d183374aceaef9609a446024a3c8d2d
code <carlos.oliveira.dev.junior@gmail.com>
2025-05-21 18:53:37 -0300

    form enviando requisicao post

--- src/client/main.js
+++ src/client/main.js
@@ -1,5 +1,26 @@
-const response = await fetch("http://localhost:3000/cadastrar", {
-  method: "POST",
-  body: JSON.stringify({ username: "example" }),
-  // …
+const formulario = document.getElementById('formulario')
+
+formulario.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const select = document.getElementById('corretora');
+
+      const dados = {
+      nome: form.nome.value,
+      contrato: form.contrato.value,
+      corretora: select.value
+    };
+
+    const resposta = await fetch('http://localhost:3000/cadastrar', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dados)
+    });
+
+    const resultado = await resposta.json();
+    console.log('Servidor respondeu:', resultado);
+
 });
