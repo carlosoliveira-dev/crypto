5204b3b839155781859814d28b493a8df1334ffc
code <carlos.oliveira.dev.junior@gmail.com>
2025-05-22 17:39:12 -0300

    rota cadastrando no sqlite

--- src/server/main.js
+++ src/server/main.js
@@ -1,5 +1,8 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import Database from 'better-sqlite3';
+const db = new Database('database.db');
+db.pragma('journal_mode = WAL');
 
 const app = express();
 
@@ -11,6 +14,10 @@ app.get("/hello", (req, res) => {
 
 app.post('/cadastrar', (req, res) => {
   console.log(req.body);
+
+  const insert = db.prepare('INSERT INTO criptomoedas (nome, contrato, corretora) VALUES (?,?,?)');
+  insert.run(req.body.nome, req.body.contrato, req.body.corretora);
+  
   res.send(req.body);
 })
 
